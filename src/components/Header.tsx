830bfbe8af2988e2d4f9fa83c3676bb44b2488fe
Victor Aguado Navarro <aguadonavarrovictor@gmail.com>
2024-01-22 21:17:34 +0100

    borrados .tsx

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export function Header({children}:{children: string}){
-    let title: string = children;
-    return (
-        <header className="header">
-            <h1>{title}</h1>
-        </header>
-    )
-}
